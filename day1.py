d869e2368d4807cc73bd1233e2e7a8711cedc5c5
Richard Boulton <richard@tartarus.org>
2017-12-19 08:59:42 +0000

    Day 1 part 1

---
+++ day1.py
@@ -0,0 +1,18 @@
+prog = 'R3, L2, L2, R4, L1, R2, R3, R4, L2, R4, L2, L5, L1, R5, R2, R2, L1, R4, R1, L5, L3, R4, R3, R1, L1, L5, L4, L2, R5, L3, L4, R3, R1, L3, R1, L3, R3, L4, R2, R5, L190, R2, L3, R47, R4, L3, R78, L1, R3, R190, R4, L3, R4, R2, R5, R3, R4, R3, L1, L4, R3, L4, R1, L4, L5, R3, L3, L4, R1, R2, L4, L3, R3, R3, L2, L5, R1, L4, L1, R5, L5, R1, R5, L4, R2, L2, R1, L5, L4, R4, R4, R3, R2, R3, L1, R4, R5, L2, L5, L4, L1, R4, L4, R4, L4, R1, R5, L1, R1, L5, R5, R1, R1, L3, L1, R4, L1, L4, L4, L3, R1, R4, R1, R1, R2, L5, L2, R4, L1, R3, L5, L2, R5, L4, R5, L5, R3, R4, L3, L3, L2, R2, L5, L5, R3, R4, R3, R4, R3, R1'
+
+steps = prog.split(', ')
+
+pos = [0, 0]
+d = [0, 1]
+
+for step in steps:
+    t, dist = step[0], int(step[1:])
+    if t == 'R':
+        d = [d[1], -d[0]]
+    else:
+        assert t == 'L'
+        d = [-d[1], d[0]]
+    pos[0] = pos[0] + dist * d[0]
+    pos[1] = pos[1] + dist * d[1]
+
+print(pos, abs(pos[0]) + abs(pos[1]))
