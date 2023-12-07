31a40428b81093ab690ac6ce078da96e33d80ea2
= <=>
2023-12-07 13:22:08 +0000

    Counter() is such a useful class for these

---
+++ 2023/day7b.py
@@ -0,0 +1,54 @@
+from collections import Counter
+
+input = '''
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+'''
+
+input = open('2023/input7.txt', 'r').read()
+
+cards = 'AKQT98765432J'
+
+class Hand:
+    def __init__(self, line):
+        c, b = line.strip().split(' ')
+        self.cards = c
+        self.bid = int(b)
+        self.counts = Counter(self.cards.replace('J', ''))
+        self.countcounts = sorted(Counter(self.counts).values(), reverse=True)
+        self.jokers = len(self.cards) - sum(self.counts.values())
+        if self.jokers == 5:
+            self.countcounts = [5]
+        else:
+            self.countcounts[0] += self.jokers
+        self.cardvals = [len(cards) - cards.index(l) for l in self.cards]
+
+    def __repr__(self):
+        return "Hand<{}, {}, {}, {}>".format(
+            self.cards,
+            self.jokers,
+            self.countcounts,
+            self.cardvals,
+        )
+    
+    def __lt__(self, other):
+        if self.countcounts < other.countcounts:
+            return True
+        if self.countcounts > other.countcounts:
+            return False
+        return self.cardvals < other.cardvals
+    
+    def three_of_a_kind(self):
+        return self.countcounts[0] == 3
+        
+
+hands = [Hand(line) for line in input.strip().split('\n')]
+hands.sort()
+score = 0
+for i, h in enumerate(hands, 1):
+    print(i, h)
+    score += i * h.bid
+print(score)
